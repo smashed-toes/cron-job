a623bf25c9a27bd30129c14723d1547e86db107d
smashed_toes <samuel_masto@brown.edu>
2020-07-17 14:30:59 -0400

    event and woohoo! loglines for cron job format test

--- src/Function/src/main/java/Handler.java
+++ src/Function/src/main/java/Handler.java
@@ -1,10 +1,14 @@
 import java.util.HashMap;
+import java.util.logging.Logger;
+
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.google.gson.Gson;
 
 public class Handler {
     public Object handler(Object event) {
         Gson gson = new Gson();
         System.out.println(gson.toJson(event));
+        System.out.println("Woohoo!");
 
         return new HashMap();
     }
